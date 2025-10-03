799be94a3ed8ebb2b4ae426118e1cb3545ee625f
khadija <khadijaouanour22@gmail.com>
2025-10-04 00:40:59 +0100

    0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){ return false; }
-        char[] S = s.toCharArray();
-        char[] T = t.toCharArray();
-        Arrays.sort(S);
-        Arrays.sort(T);
-        for(int i =0; i<s.length(); i++){
-           if (S[i]!=T[i]){
-               return false;
-           }
+    if(s.length() != t.length()){
+        return false;
+    }
+    HashMap<Character,Integer> T = new HashMap<Character, Integer>();
+    HashMap<Character,Integer> S = new HashMap<Character, Integer>();
+    for (char c : t.toCharArray()){
+        if(T.containsKey(c)){
+            T.put(c,T.get(c)+1);
+        }
+        else{
+            T.put(c,1);
+        };
+    };
+     for (char c : s.toCharArray()){
+        if(S.containsKey(c)){
+            S.put(c,S.get(c)+1);
+        }
+        else{
+            S.put(c,1);
 
-       }
-       return true;
+        };
+    };
+return S.equals(T);
 
 
 
