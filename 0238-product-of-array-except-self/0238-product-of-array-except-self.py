11d7e6e1ec198874f032f0fd480eed3781389cdd
khadija <khadijaouanour22@gmail.com>
2025-10-15 22:00:15 +0100

    0238-product-of-array-except-self

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,15 +2,14 @@ class Solution(object):
     def productExceptSelf(self, nums):
         n = len(nums)
         answer=[0]*n
-        prefix=[1]*n
-        suffix=[1]*n
-        prefix[0]=1
-        suffix[n-1]=1
+        prefix=[0]*n
+        suffix=[0]*n
+
+        prefix[0] = suffix[n-1] =1
         for i in range(1,n):
             prefix[i]=prefix[i-1]*nums[i-1]
         for j in range (n-2,-1,-1):
             suffix[j]= suffix[j+1]*nums[j+1]
-            print(suffix[j])
         for i in range (n):
             answer[i]=prefix[i]*suffix[i]
         return answer
