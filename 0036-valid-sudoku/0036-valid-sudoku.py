20dc10e08eebca8918a8d8b227d052c5c9c5cab8
khadija <khadijaouanour22@gmail.com>
2025-10-23 23:11:58 +0100

    0036-valid-sudoku

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,27 +1,17 @@
+from collections import defaultdict
 class Solution(object):
     def isValidSudoku(self, board):
-         #rows
-        for i in range (9):
-            n = set()
-            for j in range(9):
-                if board[i][j]!=".":
-                    if board[i][j] in n : return False 
-                    else : n.add(board[i][j])
-        #columns
-        for i in range (9):
-            n =  set()
-            for j in range(9):
-                if board[j][i]!=".":
-                    if board[j][i] in n : return False 
-                    else : n.add(board[j][i])
-        #squares 
-        data = {}
+        n = defaultdict(set)
+        m = defaultdict(set)
+        p = defaultdict(set)
         for i in range (9):
             for j in range(9):
-                key = (i//3,j//3)
-                if board[i][j]!=".":
-                    if  board[i][j] in  data.get(key, set()):
-                        return False 
-                    else : 
-                        data.setdefault(key, set()).add(board[i][j])
+                if board[i][j]==".":
+                    continue
+                if board[i][j] in n[i] or board[i][j] in m[j] or  board[i][j] in  p[(i//3,j//3)]:
+                     return False 
+                n[i].add(board[i][j])
+                m[j].add(board[i][j])
+                p[(i//3,j//3)].add(board[i][j])
+    
         return True
