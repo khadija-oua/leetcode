a7b9016380ba4cc00ba171063649799350ace146
khadija <khadijaouanour22@gmail.com>
2025-11-22 16:01:33 +0100

    0001-two-sum 0 ms (100%) 18.9 MB (34.04%) Easy

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m={}
         for i in range(len(nums)):
-            x=target-nums[i]
-            if x in nums and nums.index(x)!=i:
-                return [i,nums.index(x)]
+            x = target-nums[i]
+            if x in m:
+                return [i,m[x]]
+            m[nums[i]]=i
